64277face93fbc1f7dd2b6be78bc9f12f7e461b3
TerrifyingAnt <TerrifyingAnt@yandex.ru>
2023-12-17 23:10:06 +0300

    ya+docker+test

--- 1/app/settings.py
+++ 1/app/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     amqp_url: str
     postgres_url: str
+    port: str
     model_config = SettingsConfigDict(env_file='.env')
 
 
