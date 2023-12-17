18616bf38b81caa5e02db5ecbe7ca678f08ea98a
TerrifyingAnt <TerrifyingAnt@yandex.ru>
2023-12-18 01:06:02 +0300

    monitoring

--- 1/main.py
+++ 1/main.py
@@ -2,9 +2,14 @@ import asyncio
 from fastapi import FastAPI
 from app import rabbitmq_consume
 from app.routers.order_route import order_router
+from fastapi_prometheus_exporter import PrometheusExporterMiddleware
 
 app = FastAPI(title='Orders')
 
+PrometheusExporterMiddleware.setup(
+    app=app,
+    metrics_path="/metrics"
+)
 
 @app.on_event('startup')
 async def startup():
